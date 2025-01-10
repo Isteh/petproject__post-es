982ef9b1aabf6f01ba51cfffd609e8e7d88e5dd4
Isteh <isteh.code@gmail.com>
2025-01-10 19:26:53 +0400

    add CI

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,6 +3,7 @@ import webpack, { WebpackPluginInstance } from 'webpack';
 import { BuildOptions } from './types/config';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 export function buildPlugins(options: BuildOptions): WebpackPluginInstance[] {
   const { paths } = options;
@@ -22,7 +23,8 @@ export function buildPlugins(options: BuildOptions): WebpackPluginInstance[] {
   if (options.isDev) {
     plugins.push(
       new ReactRefreshWebpackPlugin(),
-      new webpack.HotModuleReplacementPlugin()
+      new webpack.HotModuleReplacementPlugin(),
+      new BundleAnalyzerPlugin({ openAnalyzer: false })
     );
   }
 
